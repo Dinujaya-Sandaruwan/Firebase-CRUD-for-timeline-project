22b32dfb562fa5f4d17872b3a29400726516e780
Aruna Sampath <88492493+Dinujaya-Sandaruwan@users.noreply.github.com>
2023-06-18 21:08:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Preview from './components/preview/Preview';
+
+function App() {
+
+    return (
+        <div className="App">
+            <Preview />
+        </div>
+    );
+}
+
+export default App;
